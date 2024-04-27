6d05f89d9a712ffa343efe4f7b64130eb6bc97aa
Aayush Bhadula <105729140+aayush-a27@users.noreply.github.com>
2024-04-27 11:46:37 +0000

    styles

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,42 +5,61 @@ import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
 
 const MeetingTypeList = () => {
-    const router=useRouter();
-    const [meetingState, setmeetingState] = useState<"isScheduleMeeting"|"isJoiningMeeting"|"isInstantMeeting"|undefined>()
+    const router = useRouter();
+    const [meetingState, setmeetingState] = useState<"isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined>()
 
-  return (
-    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-        <HomeCard
-            img="/icons/add-meeting.svg"
-            title="New Meeting"
-            description="Start an instant meeting"
-            handleClick={()=>setmeetingState("isJoiningMeeting")
-            }
-            className="bg-orange-1"
-            />
-        <HomeCard
-            img="/icons/schedule.svg"
-            title="Schedule Meeting"
-            description="Plan your meeting"
-            handleClick={()=>setmeetingState("isScheduleMeeting")}
-            className="bg-blue-1"
-        />
-        <HomeCard
-            img="/icons/recordings.svg"
-            title="view Recording"
-            description="Check out your Recordings"
-            handleClick={()=>setmeetingState("isJoiningMeeting")}
-            className="bg-purple-1"
-        />
-        <HomeCard
-            img="/icons/join-meeting.svg"
-            title="New Meeting"
-            description="Start an instant meeting"
-            handleClick={()=>router.push('/recordings')}
-            className="bg-yellow-1"
-        />
-    </section>
-  )
+    return (
+        <section className=" h-screen flex size-full flex-col gap-10 text-white overflow-y-hidden">
+            <div className='h-1/3 w-full flex justify-center items-center gap-10'>
+                <div className='h-full w-1/3 border-2 rounded-xl border-black'>
+                    <HomeCard
+                        img="/icons/add-meeting.svg"
+                        title="New Meeting"
+                        description="Start an instant meeting"
+                        handleClick={() => setmeetingState("isJoiningMeeting")
+                        }
+                        className="bg-orange-1"
+                    />
+                </div>
+                <div className='h-full w-3/5 border-2 rounded-xl border-black'>
+                    <HomeCard
+                        img="/icons/schedule.svg"
+                        title="Schedule Meeting"
+                        description="Plan your meeting"
+                        handleClick={() => setmeetingState("isScheduleMeeting")}
+                        className="bg-blue-1"
+                    />
+                </div>
+
+
+            </div>
+
+            <div className='h-1/3 w-full flex justify-center items-center gap-10'>
+                <div className='h-full w-3/5 border-2 rounded-xl border-black'>
+                    <HomeCard
+                        img="/icons/recordings.svg"
+                        title="view Recording"
+                        description="Check out your Recordings"
+                        handleClick={() => setmeetingState("isJoiningMeeting")}
+                        className="bg-purple-1"
+                    />
+                </div>
+
+                <div className='h-full w-1/3 border-2 rounded-xl border-black'>
+                    <HomeCard
+                        img="/icons/join-meeting.svg"
+                        title="New Meeting"
+                        description="Start an instant meeting"
+                        handleClick={() => router.push('/recordings')}
+                        className="bg-yellow-1"
+                    />
+                </div>
+
+            </div>
+
+
+        </section>
+    )
 }
 
 export default MeetingTypeList
