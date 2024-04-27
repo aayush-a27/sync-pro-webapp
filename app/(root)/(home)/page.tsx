897da417f409d244906aa51f65603447f9eba80f
Aayush Bhadula <105729140+aayush-a27@users.noreply.github.com>
2024-04-27 12:52:22 +0000

    Home

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,26 +1,19 @@
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
-   const now = new Date();
+  const now = new Date();
 
-   const time = now.toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata', hour: '2-digit', minute: '2-digit' });
-   const date = (new Intl.DateTimeFormat('en-IN', { timeZone: 'Asia/Kolkata', dateStyle: 'full' })).format(now);
+  const time = now.toLocaleTimeString('en-IN', { timeZone: 'Asia/Kolkata', hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-IN', { timeZone: 'Asia/Kolkata', dateStyle: 'full' })).format(now);
   return (
     <section className="flex size-full flex-col gap-5 text-white">
-    <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-      <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-        <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-          Upcoming Meeting at: 12:30 PM
-        </h2>
-        <div className="flex flex-col gap-2">
-          <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-          <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
-        </div>
+      <div className='h-14 flex px-10 justify-between items-center gap-20'>
+        <div className='h-full w-2/12 text-black font-extrabold flex justify-center items-center border rounded-xl border-black bg-white'>{time}</div>
+        <div className='h-full w-2/12 text-black font-extrabold flex justify-center items-center border rounded-xl border-black bg-white'>{date}</div>
       </div>
-    </div>
 
-    <MeetingTypeList/>
-  </section>
+      <MeetingTypeList />
+    </section>
   )
 }
 
