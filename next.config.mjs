2127a21510dc4c980e8f655174d9b8d79004b041
Aayush Bhadula <105729140+aayush-a27@users.noreply.github.com>
2024-04-28 08:25:50 +0000

    meetingId working

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+          allowedForwardedHosts: ['my-forwarded-host.com'],
+        },
+      },
+};
+
+// experimental: {
+//     serverActions: {
+//       // edit: updated to new key. Was previously `allowedForwardedHosts`
+//       allowedOrigins: ['my-forwarded-host.com'],
+//     },
+//   },
 
 export default nextConfig;
